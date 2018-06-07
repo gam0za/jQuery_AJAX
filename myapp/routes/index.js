3f5cc60cec74923df4b7b6f9dd05bfb61021d14c
DS-PC219-207\DS <tadpolejjin95@gmail.com>
2018-06-08 03:27:39 +0900

    routing

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// ID 중복 검사하여 그 결과를 참/거짓으로 반환
+router.get('/isUsableID',function(req,res,next){
+  
+});
+
+// 현재 존재하는 아이디 배열
+router.get('/displayIDs',function(req,res,next){
+
+});
+
+// userID DB에 추가
+router.post('/memberJoinDo',function(req,res,next){
+
+});
+
 module.exports = router;
